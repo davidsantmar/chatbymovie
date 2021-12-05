0725ce8360963087d29acdd65608301e1adf7755
David <davidsantmar@gmail.com>
2021-12-05 15:24:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './redux/components/Header';
+import Instructions from './redux/components/Instructions';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Instructions />
+    </>
   );
 }
 
---
+++ src/redux/components/Instructions.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Instructions = () => {
+    return (
+        <div className="instructions__and__rules">
+                Type a movie title and you'll enter into the regarding channel.
+                Keep in mind that:
+                <ol>
+                    <li>You can't make spoiler for any movie (don't advance scenes or events!)</li>
+                    <li>Be polites</li>
+                    <li>You can report people who don't respect rules 1 or 2</li>
+                    <li>You can like or dislike comments and movies</li>
+                </ol>
+            </div>
+    );
+};
+
+export default Instructions;
